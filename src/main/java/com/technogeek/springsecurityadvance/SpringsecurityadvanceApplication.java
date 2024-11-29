62e08d9b948c5b6f70d481b58a5997d9e005e884
Vivek Shukla <vivekshukla9100@gmail.com>
2024-11-29 15:51:30 +0530

    An advanced Spring Security project demonstrating secure user authentication with dynamic salt and advanced password encoding algorithms. It validates users by securely storing and retrieving credentials from a database, ensuring robust identity verification during login.

---
+++ src/main/java/com/technogeek/springsecurityadvance/SpringsecurityadvanceApplication.java
@@ -0,0 +1,13 @@
+package com.technogeek.springsecurityadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityadvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityadvanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technogeek/springsecurityadvance/SpringsecurityadvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technogeek.springsecurityadvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityadvanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
